d6552d5c490cfd22a82be9b754e9c700dfcc7996
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-04-19 18:42:45 +0530

    Changes to be committed: 	modified:   src/pages/Home/Home.css 	modified:   src/pages/Home/Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,15 +8,22 @@ import TitleCards from '../../components/TitleCards/TitleCards'
 
 const Home = () => {
   return (
-    <div className='home'>
-      <Navbar />
-          <p>A Quick Brown Fox Jumps Over The Lazy Dog</p>
-          <div className="div-btns">
-            <TitleCards />
-          </div>
-    </div>
+    <div>
+      <div className='home'>
+        <Navbar />
+            <p>A Quick Brown Fox Jumps Over The Lazy Dog</p>
+            <div className="div-btns">
+              <TitleCards />
+            </div>
+      </div>
+      <div>
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+        <TitleCards />
+      </div>
+    </div>  
   )
 }
-
 
 export default Home
