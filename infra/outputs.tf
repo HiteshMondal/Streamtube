94a15d4bdaaa8c68cebf88352e951da15283f9c4
Hitesh <mehiteshmondal@gmail.com>
2025-08-05 19:13:04 +0530

    modified:   infra/main.tf 	modified:   infra/outputs.tf 	modified:   infra/terraform.tfvars 	modified:   infra/variables.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,7 +1,11 @@
 output "instance_id" {
-  value = aws_instance.example.id
+  value = aws_instance.ubuntu.id
 }
 
 output "instance_public_ip" {
-  value = aws_instance.example.public_ip
+  value = aws_instance.ubuntu.public_ip
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
 }
